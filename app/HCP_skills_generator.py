8ccc298dd69784d76316a20009d3a52c99b6e275
jrizzle4shizzle <jrichmond68@gmail.com>
2014-05-17 14:14:02 -0400

    completed HCP skills generator (adult and infant)

--- app/HCP_skills_generator.py
+++ app/HCP_skills_generator.py
@@ -13,11 +13,16 @@ def generate_pdf(student_name="",test_date="",instructor_name=""):
   packet = StringIO.StringIO()
   # create a new PDF with Reportlab
   can = canvas.Canvas(packet, pagesize=(605.8, 785.86))
-  #move origin up and to the left
-  #can.translate(20,24)
+
 
   can.drawString(113, 665, student_name)
   can.drawString(450, 665, test_date)
+  can.drawString(146, 71, instructor_name)
+  can.drawString(84, 55, test_date)
+
+  #pass adult CPR and AED
+  can.ellipse(298,654, 325,642)
+  can.ellipse(298,638, 325,626)
 
   #check some boxes
 
@@ -38,15 +43,35 @@ def generate_pdf(student_name="",test_date="",instructor_name=""):
   can.drawString(494, 219, u"✓")
   can.drawString(538, 219, u"✓")
 
-  can.drawString(146, 71, instructor_name)
-  can.drawString(84, 55, test_date)
-
   #next page!
   can.showPage()
-  #move origin up and to the left
-  #can.translate(20,24)
 
+  can.drawString(113, 662, student_name)
+  can.drawString(450, 662, test_date)
+  can.drawString(146, 79, instructor_name)
+  can.drawString(84, 63, test_date)
 
+  #pass adult CPR and AED
+  can.ellipse(346,654, 373,642)
+  can.ellipse(346,631, 373,619)
+  can.ellipse(346,619, 373,607)
+
+  #check some boxes
+
+  can.drawString(516, 538, u"✓")
+  can.drawString(516, 519, u"✓")
+  can.drawString(516, 505, u"✓")
+  can.drawString(540, 476, u"✓")
+  can.drawString(540, 452, u"<18")
+  can.drawString(540, 433, u"✓")
+  can.drawString(540, 411, u"✓")
+  can.drawString(540, 397, u"✓")
+  can.drawString(516, 344, u"✓")
+  can.drawString(494, 289, u"✓")
+  can.drawString(538, 289, u"✓")
+
+  can.drawString(494, 239, u"<9")
+  can.drawString(538, 239, u"<9")
 
   can.save()
 
@@ -57,17 +82,19 @@ def generate_pdf(student_name="",test_date="",instructor_name=""):
   #open up template
   output = PdfFileWriter()
   dir = os.path.realpath('.')
-  filename = os.path.join(dir, 'templates','HCP_adult_skills.pdf')
-  skills_template = PdfFileReader(file(filename, "rb"))
+  as_filename = os.path.join(dir, 'templates','HCP_adult_skills.pdf')
+  adult_skills_template = PdfFileReader(file(as_filename, "rb"))
+  is_filename = os.path.join(dir, 'templates','HCP_infant_skills.pdf')
+  infant_skills_template = PdfFileReader(file(is_filename, "rb"))
 
   #merge template with mask
 
-  page = skills_template.getPage(0)
+  page = adult_skills_template.getPage(0)
   page.mergePage(mask.getPage(0))
   output.addPage(page)
-#  page2 = skills_template.getPage(1)
-#  page2.mergePage(mask.getPage(1))
-#  output.addPage(page2)
+  page2 = infant_skills_template.getPage(0)
+  page2.mergePage(mask.getPage(1))
+  output.addPage(page2)
 
 
   #write combined pdf to file
